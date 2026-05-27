49abd1e9e8e821e1e0a4119f23cebde5ce54b9eb
Aarushi <aarushi@example.com>
2026-05-27 19:31:42 +0530

    server allow

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+server: {
+  allowedHosts: ['dsasolver-2.onrender.com']
+}
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
